718242fcf89692c1a84cb613ea55b97d3bdca03e
The1Aq <27236375+The1Aq@users.noreply.github.com>
2022-10-30 16:09:12 -0700

    added the option to start ignition and restart as well as a template for the new movement.

--- src/GameApp.java
+++ src/GameApp.java
@@ -12,6 +12,7 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import java.util.Random;
 
 interface Updatable {
     void update();
@@ -67,15 +68,38 @@ abstract class GameObject extends Group implements Updatable {
 }
 
 class Pond {
+    Random rand = new Random();
+    Circle pond;
+    public Pond(){
+        pond = new Circle(10,Color.BLUE);
+        pond.setTranslateY(rand.nextInt(700)+100);
+        pond.setTranslateX(rand.nextInt(400));
+    }
 
 }
 
 class Cloud {
+    Random rand = new Random();
+    Circle cloud;
+    int water = 0;
+    public Cloud(){
+        cloud = new Circle(rand.nextInt(80)+30,Color.WHITE);
+        cloud.setTranslateY(rand.nextInt(700)+100);
+        cloud.setTranslateX(rand.nextInt(400));
+    }
+
 
 }
 
 class PondAndCloud {
+    Cloud cloud;
+    Pond pond;
 
+    public PondAndCloud(Pane root){
+        cloud = new Cloud();
+        pond = new Pond();
+        root.getChildren().addAll(cloud.cloud,pond.pond);
+    }
 }
 
 class Helipad extends Group{
@@ -97,6 +121,7 @@ class Helipad extends Group{
 class Helicopter extends GameObject{
     int   fuel, water;
     double currSpeedY,currSpeedX;
+    boolean ignition;
     public Helicopter(){
         super();
         Circle body = new Circle(20, Color.YELLOW);
@@ -108,18 +133,20 @@ class Helicopter extends GameObject{
 
         add(body);
         add(point);
+        ignition = false;
         currSpeedX = 0;
         currSpeedY = 0;
         fuel = 0;
         water = 0;
     }
     public void move(double x,double y ){
-        if(currSpeedY <= 10)
-            currSpeedY += y;
-        if(currSpeedX <= 10)
-            currSpeedX += x;
-        if(currSpeedY < 0)
-            currSpeedX =0;
+        if(ignition) {
+            if (currSpeedY < 10)
+                currSpeedY += y;
+            if (currSpeedX < 10)
+                currSpeedX += x;
+        }
+
     }
     @Override
     public void update() {
@@ -143,12 +170,15 @@ class Game {
     }
     public void play() {
         AnimationTimer loop = new AnimationTimer() {
-
+            int count = 0;
             public void handle(long now) {
-                heli.update();
-                heli.rotate(heli.getMyRotation());
-                System.out.println(heli.currSpeedY);
-
+                count++;
+                if(count % 5 == 1) {
+                    heli.update();
+                    heli.rotate(heli.getMyRotation());
+                    System.out.println(heli.currSpeedY + " y speed");
+                    System.out.println(heli.currSpeedX + " X speed");
+                }
             }
         };
         loop.start();
@@ -164,43 +194,40 @@ public class GameApp extends Application {
 
         Pane root = new Pane();
         root.setScaleY(-1);
-
-
-
-        Helipad pad = new Helipad();
-        Helicopter boom = new Helicopter();
-        root.getChildren().addAll(pad,boom);
-
-
+        int rotation = 0;
+        init(root);
+        init(root);
+        Helicopter temp = (Helicopter) root.getChildren().get(3);
         Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT, Color.BLACK);
 
-
-        boom.translate(200,50);
-        boom.setPivot(200,50);
-
-
         primaryStage.setScene(scene);
+
         scene.setOnKeyPressed(e -> {
             if(e.getCode() == KeyCode.W){
-                boom.move(0,.1);
+                temp.move(0,.1);
             }
             if(e.getCode() == KeyCode.S){
-                boom.move(0,-.1);
+                temp.move(0,-.1);
             }
             if(e.getCode() == KeyCode.A){
-                if(boom.currSpeedY > 0.1)
-                    boom.move(-.1,0);
-                boom.rotate(boom.getMyRotation() + 15);
+                if(temp.currSpeedY > 0.1)
+                    temp.move(-.1,0);
+                temp.rotate(temp.getMyRotation() + 15);
             }
             if(e.getCode() == KeyCode.D){
-                if(boom.currSpeedY > 0.1)
-                    boom.move(.1,0);
-                boom.rotate(boom.getMyRotation() - 15);
+                if(temp.currSpeedY > 0.1)
+                    temp.move(.1,0);
+                temp.rotate(temp.getMyRotation() - 15);
             }
+            if(e.getCode() == KeyCode.I){
+                temp.ignition = true;
+            }
+            if(e.getCode() == KeyCode.R)
+                init(root);
         });
 
 
-        Game game = new Game(root,boom);
+        Game game = new Game(root,temp);
         game.play();
         primaryStage.setTitle("Rain Maker");
         // prevent window resizing by user
@@ -211,7 +238,45 @@ public class GameApp extends Application {
 
 
     } // end of start()
+    public static void calMove(int rot, Helicopter heli){
+        if(rot == 0) {
+            heli.currSpeedX = 0;
+            heli.move(0, .1);
+        } else if(rot > 0 && rot < 90){
+            heli.currSpeedX = -heli.currSpeedY;
+            heli.move(-.1,.1);
+        } else if (rot == 90) {
+            heli.currSpeedY = 0;
+            heli.move(-.1,0);
+        } else if (rot > 90 && rot < 180) {
+            System.out.println("here");
+            heli.currSpeedY = heli.currSpeedX;
+            heli.move(-.1,-.1);
+        } else if (rot == 180) {
+            heli.currSpeedX = 0;
+            heli.move(0,-.1);
+        }else if(rot > 180 && rot < 270){
+            heli.currSpeedX = -heli.currSpeedY;
+            heli.move(.1,-.1);
+        } else if (rot == 270) {
+            heli.currSpeedY = 0;
+            heli.move(.1,0);
+        } else if (rot > 270 && rot < 360) {
+            heli.currSpeedY = heli.currSpeedX;
+            heli.move(.1,.1);
+        }
+    }
+    public void init(Pane root){
+        root.getChildren().clear();
+        Helipad pad = new Helipad();
+        Helicopter boom = new Helicopter();
+        PondAndCloud com = new PondAndCloud(root);
+        root.getChildren().addAll(pad,boom);
 
+        boom.translate(200,50);
+        boom.setPivot(200,50);
+
+    }
     public static void main(String[] args) {
         Application.launch();
     }
